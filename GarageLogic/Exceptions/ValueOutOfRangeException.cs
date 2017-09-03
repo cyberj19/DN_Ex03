7d16d103fc32f3ba8e81049c8e0a791339ff96e9
Johnny Ron <yehonatanron@gmail.com>
2017-09-03 23:52:17 +0300

    Written value out of range exception

--- GarageLogic/Exceptions/ValueOutOfRangeException.cs
+++ GarageLogic/Exceptions/ValueOutOfRangeException.cs
@@ -4,7 +4,30 @@ namespace GarageLogic.Exceptions
 {
     class ValueOutOfRangeException : Exception
     {
-        float m_MaxValue;
-        float m_MinValue;
+        private const string k_ErrFormartStr = "Out of range. Range: {0} - {1}";
+        private readonly float r_MinValue;
+        private readonly float r_MaxValue;
+
+        public float Min
+        {
+            get
+            {
+                return r_MinValue;
+            }
+        }
+
+        public float Max
+        {
+            get
+            {
+                return r_MaxValue;
+            }
+        }
+
+        public ValueOutOfRangeException(float i_MinValue, float i_MaxValue) : base(string.Format(k_ErrFormartStr, i_MinValue, i_MaxValue))
+        {
+            r_MinValue = i_MinValue;
+            r_MaxValue = i_MaxValue;
+        }
     }
 }
