db680cd6cc717e67b7abaf538f7d0eb0d1987b5a
Johnny Ron <yehonatanron@gmail.com>
2017-09-03 11:16:36 +0300

    Bugfix in GetVehicle Rename of GetVehicle Implemented GetVehicles

--- GarageLogic/GarageManager.cs
+++ GarageLogic/GarageManager.cs
@@ -4,23 +4,47 @@ namespace GarageLogic
 {
     public class GarageManager
     {
-        List<VehicleRecord> m_VehicleRecord;
+        private const int k_StatusNotFound = -1;
+        List<VehicleRecord> m_VehicleRecords;
 
         public static void ShowVehiclesArrangedByRegistrationNumber()
         {
 
         }
 
-        public Vehicle GetVehicle(string plateNumber) //todo: this is only the basic = MUST!!! refactor this func!!
+        public List<VehicleRecord> GetVehicleRecords(List<VehicleRecord.eStatus> i_FilterOutList)
         {
-            Vehicle foundVehicle = null;
+            List<VehicleRecord> retVehicleList = new List<VehicleRecord>();
+
+            if (i_FilterOutList.Count == 0)
+            {
+                retVehicleList = m_VehicleRecords;
+            }
+            else
+            {
+                foreach (VehicleRecord vehicleRecord in m_VehicleRecords)
+                {
+                    if (k_StatusNotFound == i_FilterOutList.IndexOf(vehicleRecord.Status))
+                    {
+                        retVehicleList.Add(vehicleRecord);
+                    }
+                }
+            }
+                return retVehicleList;
+        }
+
+        public VehicleRecord GetVehicleRecord(string plateNumber) //todo: this is only the basic = MUST!!! refactor this func!!
+        {
+            VehicleRecord foundVehicle = null;
             int plateNumHashCode = plateNumber.GetHashCode();
 
-            foreach (VehicleRecord vr in m_VehicleRecord)
+            foreach (VehicleRecord vehicleRecord in m_VehicleRecords)
             {
-                if(vr.Vehicle.GetHashCode() == plateNumHashCode)
+                //todo: isnt .find implemented for this?
+                if(vehicleRecord.Vehicle.GetHashCode() == plateNumHashCode)
                 {
-                    foundVehicle = vr.Vehicle;
+                    foundVehicle = vehicleRecord;
+                    break;
                 }
             }
 
